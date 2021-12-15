931cff3d14a57a28ee946bef7aa55a995da01054
Fabio M R Guimaraes <fabiomrg@gmail.com>
2021-12-15 18:05:32 -0300

    ajustes_visuais

--- src/views/sections/Hero.vue
+++ src/views/sections/Hero.vue
@@ -14,19 +14,19 @@
           width="90%"
         >
         <br/><br/><br/>
-        <v-card class="rounded-card " color="#0000004D">
+        <v-card class="rounded-xl" color="#0000004D">
           <br/><br/>
-          <v-row class="mx-8" align="center">
+          <v-row class="mx-6" align="center">
             <base-subheading
             size="text-h5"
             weight="regular"
             title="Veganismo é a grande ferramenta para transformar o mundo!"  
             />
-            <base-body>
-              <p>O ser humano por séculos tem se desconectado da natureza e por consequência lógica também se afastou das relações de compaixão pela terra de seus seres.</p>
-              <p>O termo vegano já existe há 77 anos e foi criado pelo britânico Donald Watson e, desde então, transformou-se em um movimento político, ético e de estilo de vida, e vem ganhando muitos adeptos, mas hoje não é apenas isso, o veganismo é movimento.</p> 
-              <p>Mas ainda temos muito a percorrer para que o mundo alcance o alinhamento e equilíbrio necessário para sermos sustentáveis em todos os âmbitos da nossa vida.</p>
-              <p>Meu compromisso como Chef de gastronomia exclusivamente vegana é pensar, falar e praticar o veganismo, somente uma consciência livre de sofrimento vai transformar o planeta!</p>
+            <base-body >
+              <p class="text-white">O ser humano por séculos tem se desconectado da natureza e por consequência lógica também se afastou das relações de compaixão pela terra de seus seres.</p>
+              <p class="text-white">O termo vegano já existe há 77 anos e foi criado pelo britânico Donald Watson e, desde então, transformou-se em um movimento político, ético e de estilo de vida, e vem ganhando muitos adeptos, mas hoje não é apenas isso, o veganismo é movimento.</p> 
+              <p class="text-white">Mas ainda temos muito a percorrer para que o mundo alcance o alinhamento e equilíbrio necessário para sermos sustentáveis em todos os âmbitos da nossa vida.</p>
+              <p class="text-white">Meu compromisso como Chef de gastronomia exclusivamente vegana é pensar, falar e praticar o veganismo, somente uma consciência livre de sofrimento vai transformar o planeta!</p>
 
             </base-body>
           </v-row>
@@ -55,9 +55,12 @@
     },
   }
 </script>
-<style scoped>
-  .rounded-card{
-      border-radius:20px;
+<style type="text/css">
+  rounded-card{
+      border-radius:60;
+  }
+  p{
+      color:white;
   }
 
 </style>
